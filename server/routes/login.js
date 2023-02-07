147acf5cb04579367e78034838308ae09d80c8bb
kimwanyoung <dhks2869@gmail.com>
2023-02-07 16:04:26 +0900

    fix : userId undefined fix

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/', async (req, res) => {
     const userId = req.body.data.id;
     const userPw = req.body.data.password;
+    if (userId === undefined) return res.json('등록되지 않은 유저 입니다.');
     if (userId === 'admin') return res.json('success');
     connection.query(`
         select userId, userPw from ${LOGIN_SHEET} where userId = ?;
