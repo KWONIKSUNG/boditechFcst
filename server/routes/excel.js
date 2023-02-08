0a49b9413dc890d101a7d037e158fd09961637b0
kimwanyoung <dhks2869@gmail.com>
2023-02-08 16:22:12 +0900

    fix : column명 수정

--- server/routes/excel.js
+++ server/routes/excel.js
@@ -5,7 +5,6 @@ const { SHEET_NAME, GET_SHEET_NAME } = require('../common/var');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  console.log(req.query.agency);
   connection.query(`
         select
           Year, Name, Company, Catalog, Cat_no, Unit, January,February,March,Aprill,May,June,July,August,September,October,November,December
@@ -16,7 +15,6 @@ router.get('/', async (req, res) => {
         order by 
           Year desc
   `, (error, rows, filelds) => {
-    console.info(rows);
     res.json([{ data: rows }]);
   })
 })
@@ -24,16 +22,14 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const newArr = req.body.data.form;
   const arrToSql = [];
-
   for (let i = 0; i < newArr.length; i++) {
+
     let cat_no = newArr[i][4];
     let agency = newArr[i][2];
     let UpdateDate = newArr[i][0];
     const param = newArr[i];
     const zeroCount = param.filter(elem => elem === 0 || elem === '0').length;
-    console.log(zeroCount);
     if (zeroCount > 13) continue;
-    // const value = param.map(prop => "'" + prop + "'");
     param.push('date(now())');
     const convertValue = param.map(prop => {
       if (isNaN(Number(prop))) {
@@ -52,16 +48,17 @@ router.post('/', async (req, res) => {
 
   arrToSql.forEach((props) => {
     const query = `
-    insert into ${SHEET_NAME} values (?) on duplicate key update UpdateDate = ${props.convertValue[0]}, userName='${props.convertValue[1]}', agency='${props.convertValue[2]}', catalog='${props.convertValue[3]}', cat_no='${props.convertValue[4]}', unit='${props.convertValue[5]}', January=${props.convertValue[6]},February=${props.convertValue[7]},March=${props.convertValue[8]},Aprill=${props.convertValue[9]},May=${props.convertValue[10]},June=${props.convertValue[11]},July=${props.convertValue[12]},August=${props.convertValue[13]},September=${props.convertValue[14]},October=${props.convertValue[15]},November=${props.convertValue[16]},December=${props.convertValue[17]}, InsertDate='${props.convertValue[18]}'
+    insert into ${SHEET_NAME} values (?) on duplicate key update Year = ${props.convertValue[0]}, Name='${props.convertValue[1]}', Company='${props.convertValue[2]}', Catalog='${props.convertValue[3]}', Cat_no='${props.convertValue[4]}', Unit='${props.convertValue[5]}', January=${props.convertValue[6]},February=${props.convertValue[7]},March=${props.convertValue[8]},Aprill=${props.convertValue[9]},May=${props.convertValue[10]},June=${props.convertValue[11]},July=${props.convertValue[12]},August=${props.convertValue[13]},September=${props.convertValue[14]},October=${props.convertValue[15]},November=${props.convertValue[16]},December=${props.convertValue[17]}, InsertDate='${props.convertValue[18]}'
   `;
     connection.query(query, [props.convertValue], (error, rows, fields) => {
       if (error) {
-        return console.info(error);
+        return;
       }
     })
   })
 
-  return res.json([{ data: 'upload success' }]);
+  return res.send([{ data: 'upload success' }]);
+
 })
 
 module.exports = router;
