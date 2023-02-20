7890a38086cff9ec6d03404314f08fba0f557773
kimwanyoung <dhks2869@gmail.com>
2023-02-20 10:30:07 +0900

    add : redux-persist, hashrouter

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Sheet from "./pages/Sheet";
 import Login from "./pages/Login";
 import Admin from "./pages/Admin";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/Sheet" element={<Sheet />} />
         <Route path="/" element={<Login />} />
         <Route path="/Admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
